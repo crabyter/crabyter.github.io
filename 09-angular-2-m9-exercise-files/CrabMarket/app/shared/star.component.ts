56f4f6071353b81a3bc0c5e82ce535af71c12576
yazheng, wang <wangyz@sinyoo.net>
2017-02-16 17:23:16 +0800

    更新5星评价显示问题
    5星评价显示换行了,加大5星的div长度,以便不换行显示

--- 09-angular-2-m9-exercise-files/CrabMarket/app/shared/star.component.ts
+++ 09-angular-2-m9-exercise-files/CrabMarket/app/shared/star.component.ts
@@ -13,7 +13,7 @@ export class StarComponent implements OnChanges {
         new EventEmitter<string>();
 
     ngOnChanges(): void {
-        this.starWidth = this.rating * 86 / 5;
+        this.starWidth = this.rating * 96 / 5;
     }
 
     onClick(): void {
